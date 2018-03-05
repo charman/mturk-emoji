4bbc412cdf42eac4e45ff24d7cf725a054c7ef60
Craig Harman <craig@craigharman.net>
2018-03-05 16:53:31 -0500

    Refactored Python code, improved documentation

--- encode_emoji.py
+++ encode_emoji.py
@@ -8,24 +8,37 @@ import re
 import sys
 
 
-def emoji_match_to_span(emoji_match):
-    bytes = codecs.encode(emoji_match.group(), 'utf-8')
-    bytes_as_json = json.dumps([ord(b) for b in bytes])
-    return "<span class='emoji-bytes' data-emoji-bytes='%s'></span>" % bytes_as_json
-    return emoji
-
-
 def replace_emoji_characters(s):
-    """
-    Mechanical Turk will reject CSV input files containing Emoji
-    characters with this error message:
+    """Replace 4-byte characters with HTML spans with bytes as JSON array
 
-      Your CSV file needs to be UTF-8 encoded and cannot contain
-      characters with encodings larger than 3 bytes.
+    This function takes a Unicode string containing 4-byte Unicode
+    characters, e.g. 😀, and replaces each 4-byte character with an
+    HTML span with the 4 bytes encoded as a JSON array, e.g.:
+
+      <span class='emoji-bytes' data-emoji-bytes='[240, 159, 152, 128]'></span>
+
+    Args:
+        s (Unicode string):
+    Returns:
+        Unicode string with all 4-byte Unicode characters in the source
+        string replaced with HTML spans
     """
-    # The procedure for stripping Emoji characters is based on this StackOverflow post:
+    def _emoji_match_to_span(emoji_match):
+        """
+        Args:
+            emoji_match (MatchObject):
+
+        Returns:
+            Unicode string
+        """
+        bytes = codecs.encode(emoji_match.group(), 'utf-8')
+        bytes_as_json = json.dumps([ord(b) for b in bytes])
+        return u"<span class='emoji-bytes' data-emoji-bytes='%s'></span>" % \
+            bytes_as_json
+
+    # The procedure for stripping Emoji characters is based on this
+    # StackOverflow post:
     #   http://stackoverflow.com/questions/12636489/python-convert-4-byte-char-to-avoid-mysql-error-incorrect-string-value
-
     if sys.maxunicode == 1114111:
         # Python was built with '--enable-unicode=ucs4'
         highpoints = re.compile(u'[\U00010000-\U0010ffff]')
@@ -33,9 +46,10 @@ def replace_emoji_characters(s):
         # Python was built with '--enable-unicode=ucs2'
         highpoints = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
     else:
-        raise UnicodeError("Unable to determine if Python was built using UCS-2 or UCS-4")
+        raise UnicodeError(
+            "Unable to determine if Python was built using UCS-2 or UCS-4")
 
-    return highpoints.sub(emoji_match_to_span, s)
+    return highpoints.sub(_emoji_match_to_span, s)
 
 
 def main():
